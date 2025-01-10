1b500bd138c4b08e7659418fd2cbbd561cb1728c
QutaqKicker <56227723+QutaqKicker@users.noreply.github.com>
2025-01-10 23:51:01 +0300

    adding kafka constants

--- Common/constants/constants.go
+++ Common/constants/constants.go
@@ -1,9 +1,15 @@
 package constants
 
 const (
-	AuditPortEnvName  = "ChatParser_Audit_Port"
-	BackupPortEnvName = "ChatParser_Backup_Port"
-	ChatPortEnvName   = "ChatParser_Chat_Port"
-	UserPortEnvName   = "ChatParser_User_Port"
-	RouterPortEnvName = "ChatParser_Router_Port"
+	AuditPortEnvName       = "ChatParser_Audit_Port"
+	BackupPortEnvName      = "ChatParser_Backup_Port"
+	ChatPortEnvName        = "ChatParser_Chat_Port"
+	UserPortEnvName        = "ChatParser_User_Port"
+	RouterPortEnvName      = "ChatParser_Router_Port"
+	KafkaBroker1UrlEnvName = "ChatParser_Kafka_1_Url"
+)
+
+const (
+	KafkaAuditCreateLogTopicName     = "audit-create-log"
+	KafkaUserMessageCounterTopicName = "user-message-counter"
 )
