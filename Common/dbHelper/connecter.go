7616e293f7c88eeeeeb0bc63c1b387c84a221bf3
Sadykov Azat <sdkazt@gmail.com>
2025-01-26 16:37:28 +0300

    moving db setuper and logger to common module

---
+++ Common/dbHelper/connecter.go
@@ -0,0 +1,31 @@
+package dbHelper
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+type DbConfig struct {
+	Host     string `yaml:"host" env-required:"true"`
+	Port     int    `yaml:"port" env-required:"true"`
+	User     string `yaml:"user" env-required:"true"`
+	Password string `yaml:"password" env-required:"true"`
+	DbName   string `yaml:"dbName" env-required:"true"`
+}
+
+func connectDb(cfg DbConfig) (*sql.DB, error) {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.DbName)
+	db, err := sql.Open("postgres", psqlInfo)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
---
+++ Common/myLogs/setuper.go
@@ -0,0 +1,46 @@
+package myLogs
+
+import (
+	"context"
+	"fmt"
+	"github.com/QutaqKicker/ChatParser/Common/myKafka"
+	"log/slog"
+	"time"
+)
+
+func setupLogger(producer *myKafka.AuditProducer, serviceName string) *slog.Logger {
+	log := slog.New(&AuditLogHandler{producer: producer, serviceName: serviceName})
+	return log
+}
+
+type AuditLogHandler struct {
+	producer    *myKafka.AuditProducer
+	serviceName string
+}
+
+func (h *AuditLogHandler) Handle(ctx context.Context, record slog.Record) error {
+	fmt.Println(record.Message)
+
+	err := h.producer.Send(myKafka.CreateLogRequest{
+		ServiceName: h.serviceName,
+		Type:        int32(record.Level),
+		Message:     record.Message,
+		Created:     time.Now(),
+	})
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	return err
+}
+
+func (h *AuditLogHandler) Enabled(ctx context.Context, level slog.Level) bool {
+	return true
+}
+func (h *AuditLogHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	return h
+}
+
+func (h *AuditLogHandler) WithGroup(name string) slog.Handler {
+	return h
+}
